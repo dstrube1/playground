5014e447ce04289256762dc7113cfd10d7688602
David Strube <dstrube@gmail.com>
2017-08-03 07:09:51 -0400

    Android - Auto Complete

---
+++ Android/TestAutoComplete/src/com/dstrube/testautocomplete/MainActivity.java
@@ -0,0 +1,94 @@
+package com.dstrube.testautocomplete;
+
+import android.app.Activity;
+//import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+//import android.os.Build;
+
+//////////////////
+/* 
+* TestAutoComplete
+* The String[] array is the source of autocomplete options
+*/
+//////////////////
+public class MainActivity extends Activity {
+
+	String[] array = {"New York","New Hampshire","New Jersey","New Mexico","New Georgia"};
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	try{
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_main);
+	
+	        if (savedInstanceState == null) {
+	            getFragmentManager().beginTransaction()
+	                    .add(R.id.container, new PlaceholderFragment())
+	                    .commit();
+	        }
+    	}
+    	catch (Exception e){
+    		Toast.makeText(getApplicationContext(), "Error: "+e.toString(), Toast.LENGTH_LONG).show();
+    	}
+    }
+
+    @Override
+    protected void onResume() {
+    	try{
+            super.onResume();
+        AutoCompleteTextView actv = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);
+    		ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.select_dialog_item, array);
+    		actv.setThreshold(1);
+    		actv.setAdapter(adapter);
+    	}
+    	catch (Exception e){
+    		Toast.makeText(getApplicationContext(), "Error: "+e.toString(), Toast.LENGTH_LONG).show();
+    	}
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
