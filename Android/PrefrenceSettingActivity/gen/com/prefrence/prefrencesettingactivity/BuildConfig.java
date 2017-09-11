e68b83f61701bf049bdfc19c0fcccb3d85c8e34c
David Strube <dstrube@gmail.com>
2017-09-11 13:39:33 -0400

    Android - Preference setting

---
+++ Android/PrefrenceSettingActivity/gen/com/prefrence/prefrencesettingactivity/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.prefrence.prefrencesettingactivity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
