8c29113973d35c07f82a53825337537d3f57c4c6
David Strube <dstrube@gmail.com>
2018-01-15 23:16:04 -0500

    Updated to run in Android Studio

--- Android/DynamicXMLBaseView/app/build/generated/source/buildConfig/debug/com/dstrube/dynamicxmlbaseview/app/BuildConfig.java
+++ Android/DynamicXMLBaseView/app/build/generated/source/buildConfig/debug/com/dstrube/dynamicxmlbaseview/app/BuildConfig.java
@@ -5,7 +5,7 @@ package com.dstrube.dynamicxmlbaseview.app;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "com.dstrube.dynamicxmlbaseview.app";
+  public static final String APPLICATION_ID = "com.dstrube.dynamicxmlbaseview.app";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
