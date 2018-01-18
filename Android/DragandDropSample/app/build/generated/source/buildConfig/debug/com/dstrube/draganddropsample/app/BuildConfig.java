008a36093c70fa819e420dc5e7b130575af8babe
David Strube <dstrube@gmail.com>
2018-01-18 11:55:19 -0500

    Updated to run in Android Studio

--- Android/DragandDropSample/app/build/generated/source/buildConfig/debug/com/dstrube/draganddropsample/app/BuildConfig.java
+++ Android/DragandDropSample/app/build/generated/source/buildConfig/debug/com/dstrube/draganddropsample/app/BuildConfig.java
@@ -5,7 +5,7 @@ package com.dstrube.draganddropsample.app;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "com.dstrube.draganddropsample.app";
+  public static final String APPLICATION_ID = "com.dstrube.draganddropsample.app";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
