fd66454d9ac3ab776f7c97a58b8b9d3ada2195a3
David Strube <dstrube@gmail.com>
2018-01-21 08:11:12 -0500

    Updated to run in Android Studio

--- Android/DetectWifi/app/build/generated/source/buildConfig/debug/com/dstrube/detectwifi/app/BuildConfig.java
+++ Android/DetectWifi/app/build/generated/source/buildConfig/debug/com/dstrube/detectwifi/app/BuildConfig.java
@@ -5,7 +5,7 @@ package com.dstrube.detectwifi.app;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "com.dstrube.detectwifi.app";
+  public static final String APPLICATION_ID = "com.dstrube.detectwifi.app";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
