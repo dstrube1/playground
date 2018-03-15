deb0549b377d8a14ffc6f4ac0ea2f7a9294afddc
David Strube <dstrube@gmail.com>
2018-03-14 20:59:29 -0400

    Still needs work

---
+++ Android/InstagramTest/app/src/main/java/com/dstrube/instagramtest/MainActivity.java
@@ -0,0 +1,33 @@
+package com.dstrube.instagramtest;
+
+import android.graphics.Point;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+    InstagramDataSource instagramDataSource;
+    InstagramAdapter instagramAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        instagramDataSource = InstagramDataSource.getInstance();
+        instagramDataSource.getMoreData();
+
+        instagramAdapter = new InstagramAdapter(this, instagramDataSource, getScreenWindow());
+        GridView gridView = findViewById(R.id.gridView);
+        gridView.setOnScrollListener(instagramAdapter);
+        gridView.setAdapter(instagramAdapter);
+    }
+
+    public int getScreenWindow(){
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        return size.x;
+    }
+}
