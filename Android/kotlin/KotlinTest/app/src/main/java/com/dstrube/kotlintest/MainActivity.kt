064fb7010991809b76845709b62e12e9395e0aa5
David Strube <dstrube@gmail.com>
2018-04-15 23:39:12 -0400

    KotlinTest

---
+++ Android/kotlin/KotlinTest/app/src/main/java/com/dstrube/kotlintest/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.dstrube.kotlintest
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //TODO Test some kotlin syntax
+}
