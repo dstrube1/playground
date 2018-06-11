c85e71cd7f2812a3d07f42bbe235784e606ae598
David Strube <dstrube@gmail.com>
2018-06-10 20:31:01 -0400

    Minor edits

--- Android/TestAutoComplete/app/src/main/java/com/dstrube/testautocomplete/MainActivity.java
+++ Android/TestAutoComplete/app/src/main/java/com/dstrube/testautocomplete/MainActivity.java
@@ -8,13 +8,13 @@ import android.widget.Toast;
 
 //////////////////
 /*
-* TestAutoComplete
-* The String[] array is the source of autocomplete options
-*/
+ * TestAutoComplete
+ * The String[] array is the source of autocomplete options
+ */
 //////////////////
 public class MainActivity extends AppCompatActivity {
 
-    String[] array = {"New York","New Hampshire","New Jersey","New Mexico","New Georgia"};
+    String[] array = {"New York", "New Hampshire", "New Jersey", "New Mexico", "New Georgia"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +24,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onResume() {
-        try{
+        try {
             super.onResume();
-            AutoCompleteTextView actv = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);
-            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.select_dialog_item, array);
-            actv.setThreshold(1);
-            actv.setAdapter(adapter);
-        }
-        catch (Exception e){
-            Toast.makeText(getApplicationContext(), "Error: "+e.toString(), Toast.LENGTH_LONG).show();
+            AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView1);
+            ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.select_dialog_item, array);
+            autoCompleteTextView.setThreshold(1);
+            autoCompleteTextView.setAdapter(adapter);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error: " + e.toString(), Toast.LENGTH_LONG).show();
         }
     }
 }
