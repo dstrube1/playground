7b28c4a64fc8f32f6bc7f826c1038b54a2f4f8b1
David Strube <dstrube@gmail.com>
2018-07-19 00:23:57 -0400

    Gradle updates, code cleanup

--- Android/IntentTest0/app/src/main/java/com/clopton/itent/MainActivity.java
+++ Android/IntentTest0/app/src/main/java/com/clopton/itent/MainActivity.java
@@ -3,72 +3,66 @@ package com.clopton.itent;
 import java.util.ArrayList;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.os.Build;
 
 public class MainActivity extends Activity {
-	
-	ArrayList<Employee> emp;
+
+    ArrayList<Employee> emp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        
-        Button startBtn = (Button)findViewById(R.id.button1);
-        emp  = new ArrayList<Employee>();
-        Employee fred = new Employee("Peter Parker","PP@gmail.com","5551234567");
+
+
+        Button startBtn = findViewById(R.id.button1);
+        emp = new ArrayList<>();
+        Employee fred = new Employee("Peter Parker", "PP@gmail.com", "5551234567");
         emp.add(fred);
-        
-        fred=new Employee("Sam Spade","SS@gmail.com","555-123-7890");
+
+        fred = new Employee("Sam Spade", "SS@gmail.com", "555-123-7890");
         emp.add(fred);
-        fred=new Employee("Clark Kent","CK@gmail.com","555-124-1230");
+        fred = new Employee("Clark Kent", "CK@gmail.com", "555-124-1230");
         emp.add(fred);
-        fred=new Employee("Nancy Drew","ND@gmail.com","555-124-7891");
+        fred = new Employee("Nancy Drew", "ND@gmail.com", "555-124-7891");
         emp.add(fred);
-        fred=new Employee("Fenton Hardy","FH@gmail.com","555-124-1235");
+        fred = new Employee("Fenton Hardy", "FH@gmail.com", "555-124-1235");
         emp.add(fred);
-        fred=new Employee("Diane Prince","DP@gmail.com","555-124-5896");
+        fred = new Employee("Diane Prince", "DP@gmail.com", "555-124-5896");
         emp.add(fred);
-        fred=new Employee("Burce Wayne","BW@gmail.com","555-124-1287");
+        fred = new Employee("Burce Wayne", "BW@gmail.com", "555-124-1287");
         emp.add(fred);
-        fred=new Employee("Dick Tracy","DT@gmail.com","555-124-8521");
+        fred = new Employee("Dick Tracy", "DT@gmail.com", "555-124-8521");
         emp.add(fred);
-        fred=new Employee("Sherlock Holmes","SH@gmail.com","555-129-1111");
+        fred = new Employee("Sherlock Holmes", "SH@gmail.com", "555-129-1111");
         emp.add(fred);
-        fred=new Employee("Hal Jorden","HJ@gmail.com","555-129-2222");
+        fred = new Employee("Hal Jorden", "HJ@gmail.com", "555-129-2222");
         emp.add(fred);
         startBtn.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent i = new Intent(MainActivity.this,Report.class);
-				i.putExtra("james", emp);
-				startActivity(i);
-			}
-		});
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(MainActivity.this, Report.class);
+                i.putExtra("james", emp);
+                startActivity(i);
+            }
+        });
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-        
-       
-        
+
+
         return true;
     }
 
@@ -80,12 +74,11 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
-            
-            
+
+
         }
         return super.onOptionsItemSelected(item);
     }
 
-   
 
 }
