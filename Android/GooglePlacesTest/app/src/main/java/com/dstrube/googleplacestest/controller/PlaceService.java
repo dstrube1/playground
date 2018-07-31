c6fa895df7a0c2a5d3531839f29b936f47570fdb
David Strube <dstrube@gmail.com>
2018-07-31 09:33:08 -0400

    Gradle updates, code cleanup

--- Android/GooglePlacesTest/app/src/main/java/com/dstrube/googleplacestest/controller/PlaceService.java
+++ Android/GooglePlacesTest/app/src/main/java/com/dstrube/googleplacestest/controller/PlaceService.java
@@ -14,88 +14,92 @@ import com.dstrube.googleplacestest.model.Place;
 
 public class PlaceService {
 
-	private String API_KEY;
+    private String API_KEY;
 
-	public PlaceService(String apikey) {
-		this.API_KEY = apikey;
-	}
+    PlaceService(String apikey) {
+        this.API_KEY = apikey;
+    }
 
-	public void setApiKey(String apikey) {
-		this.API_KEY = apikey;
-	}
+    public void setApiKey(String apikey) {
+        this.API_KEY = apikey;
+    }
 
-	private String makeUrl(double latitude, double longitude, String place) {
-		StringBuilder urlString = new StringBuilder(
-				"https://maps.googleapis.com/maps/api/place/search/json?");
+    private String makeUrl(double latitude, double longitude, String place) {
+        StringBuilder urlString = new StringBuilder(
+                "https://maps.googleapis.com/maps/api/place/search/json?");
 
-		if (place.equals("")) {
-			urlString.append("&location=");
-			urlString.append(Double.toString(latitude));
-			urlString.append(",");
-			urlString.append(Double.toString(longitude));
-			urlString.append("&radius=1000");
-			// urlString.append("&types="+place);
-			urlString.append("&sensor=false&key=" + API_KEY);
-		} else {
-			urlString.append("&location=");
-			urlString.append(Double.toString(latitude));
-			urlString.append(",");
-			urlString.append(Double.toString(longitude));
-			urlString.append("&radius=1000");
-			urlString.append("&types=" + place);
-			urlString.append("&sensor=false&key=" + API_KEY);
-		}
-		return urlString.toString();
-	}
+        if (place.equals("")) {
+            urlString.append("&location=");
+            urlString.append(Double.toString(latitude));
+            urlString.append(",");
+            urlString.append(Double.toString(longitude));
+            urlString.append("&radius=1000");
+            // urlString.append("&types="+place);
+            urlString.append("&sensor=false&key=");
+            urlString.append(API_KEY);
+        } else {
+            urlString.append("&location=");
+            urlString.append(Double.toString(latitude));
+            urlString.append(",");
+            urlString.append(Double.toString(longitude));
+            urlString.append("&radius=1000");
+            urlString.append("&types=");
+            urlString.append(place);
+            urlString.append("&sensor=false&key=");
+            urlString.append(API_KEY);
+        }
+        return urlString.toString();
+    }
 
-	protected String getJSON(String url) {
-		return getUrlContents(url);
-	}
+    protected String getJSON(String url) {
+        return getUrlContents(url);
+    }
 
-	private String getUrlContents(String theUrl) {
-		StringBuilder content = new StringBuilder();
-		try {
-			URL url = new URL(theUrl);
-			URLConnection urlConnection = url.openConnection();
-			BufferedReader bufferedReader = new BufferedReader(
-					new InputStreamReader(urlConnection.getInputStream()), 8);
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-				content.append(line + "\n");
-			}
-			bufferedReader.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return content.toString();
-	}
+    private String getUrlContents(String theUrl) {
+        StringBuilder content = new StringBuilder();
+        try {
+            URL url = new URL(theUrl);
+            URLConnection urlConnection = url.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(urlConnection.getInputStream()), 8);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                content.append(line);
+                content.append('\n');
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
 
-	public ArrayList<Place> findPlaces(double latitude, double longitude,
-			String placeSpacification) {
+    public ArrayList<Place> findPlaces(double latitude, double longitude,
+                                       String placeSpecification) {
 
-		String urlString = makeUrl(latitude, longitude, placeSpacification);
+        String urlString = makeUrl(latitude, longitude, placeSpecification);
 
-		try {
-			String json = getJSON(urlString);
+        try {
+            String json = getJSON(urlString);
 
-			System.out.println(json);
-			JSONObject object = new JSONObject(json);
-			JSONArray array = object.getJSONArray("results");
+            System.out.println(json);
+            JSONObject object = new JSONObject(json);
+            JSONArray array = object.getJSONArray("results");
 
-			ArrayList<Place> arrayList = new ArrayList<Place>();
-			for (int i = 0; i < array.length(); i++) {
-				try {
-					Place place = Place.jsonToPlace((JSONObject) array.get(i));
-					arrayList.add(place);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			return arrayList;
-		} catch (JSONException ex) {
-			ex.printStackTrace();
-		}
-		return null;
-	}
+            ArrayList<Place> arrayList = new ArrayList<>();
+            for (int i = 0; i < array.length(); i++) {
+                try {
+                    Place place = Place.jsonToPlace((JSONObject) array.get(i));
+                    arrayList.add(place);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return arrayList;
+        } catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 
 }
