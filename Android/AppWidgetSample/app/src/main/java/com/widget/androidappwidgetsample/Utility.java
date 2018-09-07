fffdfec1f9b212b75ffb5eb2168a26f872dd4db7
David Strube <dstrube@gmail.com>
2018-09-07 02:30:53 -0400

    Gradle updates, code cleanup

--- Android/AppWidgetSample/app/src/main/java/com/widget/androidappwidgetsample/Utility.java
+++ Android/AppWidgetSample/app/src/main/java/com/widget/androidappwidgetsample/Utility.java
@@ -4,10 +4,11 @@ package com.widget.androidappwidgetsample;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class Utility {
-         public static String getCurrentTime(String timeformat){
-                    Format formatter = new SimpleDateFormat(timeformat);
+         public static String getCurrentTime(String timeFormat){
+                    Format formatter = new SimpleDateFormat(timeFormat, Locale.US);
                 return formatter.format(new Date());
             }
 }
