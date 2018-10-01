0e631ee361cc314b7679d08f15d256cac6452c9b
David Strube <dstrube@gmail.com>
2018-09-30 21:01:47 -0400

    Alibi - initial creation / brainstorming

---
+++ Android/Alibi/feature/src/main/java/com/dstrube/alibi/feature/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.dstrube.alibi.feature
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    /*
+    * TODO:
+    * Ideas:
+    * 1- 3 screens:
+    *   grid
+    *   map
+    *   settings
+    * 2- grid and map will get their data from phone's local db
+    *   future feature: offload / share data?
+    * 3- settings will control data granularity- snapshot frequency (n/second, minute, hour, day
+    * 4- grid: lon, lat, city, state, country, zip, timestamp, distance from last datapoint
+    * 5- map: drawn out points
+    * 6- dark mode?
+    *
+    * requirements:
+    * 1- location setting: device, high accuracy, battery saver
+    * 2- storage of preference- has user been notified that high accuracy is best ("do not show this again"?)
+    *
+    * thoughts: this is device specific, not account specific;
+    * we don't want to tie it to an account because then multiple devices on one account will be problematic
+    * */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_grid -> {
+                // Handle the camera action
+            }
+            R.id.nav_map -> {
+
+            }
+            R.id.nav_settings -> {
+
+            }
+//            R.id.nav_manage -> {
+//
+//            }
+//            R.id.nav_share -> {
+//
+//            }
+//            R.id.nav_send -> {
+//
+//            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
