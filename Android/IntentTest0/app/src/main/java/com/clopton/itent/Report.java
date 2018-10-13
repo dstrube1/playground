c7b35b866e085de6697c1818c9bdd445e50679d9
David Strube <dstrube@gmail.com>
2018-10-13 09:45:40 -0500

    Gradle updates, code cleanup

--- Android/IntentTest0/app/src/main/java/com/clopton/itent/Report.java
+++ Android/IntentTest0/app/src/main/java/com/clopton/itent/Report.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Parcelable;
+//import android.os.Parcelable;
 import android.view.Menu;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -17,18 +17,18 @@ public class Report extends Activity{
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.report);
-		ListView lv= (ListView)findViewById(R.id.listview);
+		ListView lv= findViewById(R.id.listview);
 		  
 	        // create the grid item mapping
 	        String[] from = new String[] {"rowid", "name", "mail", "phone"};
 	        int[] to = new int[] { R.id.item1, R.id.item2, R.id.item3, R.id.item4 };
 	        ArrayList emp1 =  getIntent().getParcelableArrayListExtra("james");
 	        // prepare the list of all records
-	        List<HashMap<String, String>> fillMaps = new ArrayList<HashMap<String, String>>();
+	        List<HashMap<String, String>> fillMaps = new ArrayList<>();
 	        
 	        for(int i = 0; i < 10; i++){
 	        	Employee emp =  (Employee) emp1.get(i);
-	            HashMap<String, String> map = new HashMap<String, String>();
+	            HashMap<String, String> map = new HashMap<>();
 	            map.put("rowid", "" + i);
 	            map.put("name", emp.getName());
 	            map.put("mail", emp.getMail());
