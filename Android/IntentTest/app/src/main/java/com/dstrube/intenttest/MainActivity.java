778b99955fee448c6b18d7a63afd9a5d10477e6d
David Strube <dstrube@gmail.com>
2018-10-22 21:29:50 -0400

    Added ReportParcelableActivity

--- Android/IntentTest/app/src/main/java/com/dstrube/intenttest/MainActivity.java
+++ Android/IntentTest/app/src/main/java/com/dstrube/intenttest/MainActivity.java
@@ -11,34 +11,23 @@ import java.util.ArrayList;
 public class MainActivity extends Activity {
 
     ArrayList<Employee> employees;
+    ArrayList<Employee_P> employees_P;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] names = {"Peter Parker", "Sam Spade", "Clark Kent", "Nancy Drew", "Fenton Hardy", "Diane Prince", "Bruce Wayne", "Dick Tracy", "Sherlock Holmes", "Hal Jordan"};
+        String[] emails = {"PP@gmail.com", "SS@gmail.com", "CK@gmail.com", "ND@gmail.com", "FH@gmail.com", "DP@gmail.com", "BW@gmail.com", "DT@gmail.com", "SH@gmail.com", "HJ@gmail.com"};
+        String[] phones = {"5551234567", "555-123-7890", "555-124-1230", "555-124-7891", "555-124-1235", "555-124-5896", "555-124-1287", "555-124-8521", "555-129-1111", "555-129-2222"};
 
         employees = new ArrayList<>();
-        Employee employee = new Employee("Peter Parker", "PP@gmail.com", "5551234567");
-        employees.add(employee);
-
-        employee = new Employee("Sam Spade", "SS@gmail.com", "555-123-7890");
-        employees.add(employee);
-        employee = new Employee("Clark Kent", "CK@gmail.com", "555-124-1230");
-        employees.add(employee);
-        employee = new Employee("Nancy Drew", "ND@gmail.com", "555-124-7891");
-        employees.add(employee);
-        employee = new Employee("Fenton Hardy", "FH@gmail.com", "555-124-1235");
-        employees.add(employee);
-        employee = new Employee("Diane Prince", "DP@gmail.com", "555-124-5896");
-        employees.add(employee);
-        employee = new Employee("Burce Wayne", "BW@gmail.com", "555-124-1287");
-        employees.add(employee);
-        employee = new Employee("Dick Tracy", "DT@gmail.com", "555-124-8521");
-        employees.add(employee);
-        employee = new Employee("Sherlock Holmes", "SH@gmail.com", "555-129-1111");
-        employees.add(employee);
-        employee = new Employee("Hal Jordan", "HJ@gmail.com", "555-129-2222");
-        employees.add(employee);
+        for (int i = 0; i < names.length; i++) {
+            Employee employee = new Employee(names[i], emails[i], phones[i]);
+            employees.add(employee);
+            Employee_P employee_p = new Employee_P(names[i], emails[i], phones[i]);
+            employees_P.add(employee_p);
+        }
 
         final Button employeesSerializableButton = findViewById(R.id.employeesSerializableButton);
         employeesSerializableButton.setOnClickListener(new View.OnClickListener() {
@@ -52,15 +41,15 @@ public class MainActivity extends Activity {
             }
         });
         final Button employeesParcelableButton = findViewById(R.id.employeesParcelableButton);
-//        employeesSerializableButton.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                // TODO Auto-generated method stub
-//                Intent intent = new Intent(MainActivity.this, ReportActivity.class);
-//                intent.putExtra("james", employees);
-//                startActivity(intent);
-//            }
-//        });
+        employeesParcelableButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(MainActivity.this, ReportParcelableActivity.class);
+                intent.putExtra("james", employee_p);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ Android/IntentTest/app/src/main/java/com/dstrube/intenttest/ReportParcelableActivity.java
@@ -0,0 +1,13 @@
+package com.dstrube.intenttest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReportParcelableActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_parcelable);
+    }
+}
