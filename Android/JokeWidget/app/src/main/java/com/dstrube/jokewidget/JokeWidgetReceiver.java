47c0c80eae6beeaddaf0ce8c007895c17b1d789b
David Strube <dstrube@gmail.com>
2018-10-27 14:24:19 -0400

    Gradle updates, code cleanup

--- Android/JokeWidget/app/src/main/java/com/dstrube/jokewidget/JokeWidgetReceiver.java
+++ Android/JokeWidget/app/src/main/java/com/dstrube/jokewidget/JokeWidgetReceiver.java
@@ -10,46 +10,49 @@ import android.widget.Toast;
 //http://javatechig.com/android/app-widgets-example-in-android
 public class JokeWidgetReceiver extends BroadcastReceiver {
 
-	public static int clickCount = 0;
-	private String msg[] = null;
+    public static int clickCount = 0;
+    private String msg[] = null;
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
+    @Override
+    public void onReceive(Context context, Intent intent) {
 //		Toast.makeText(context, "begin received", Toast.LENGTH_SHORT).show();
-		if (intent.getAction().equals(WidgetUtils.WIDGET_UPDATE_ACTION)) {
-			updateWidgetPictureAndButtonListener(context);
-		}
+        final String action = intent.getAction();
+        if (action != null && action.equals(WidgetUtils.WIDGET_UPDATE_ACTION)) {
+            updateWidgetPictureAndButtonListener(context);
+        } else {
+            Toast.makeText(context, "Null action", Toast.LENGTH_SHORT).show();
+        }
 //		Toast.makeText(context, "end received", Toast.LENGTH_SHORT).show();
-	}
+    }
 
-	private void updateWidgetPictureAndButtonListener(Context context) {
-		Toast.makeText(context, "begin updateWidgetPictureAndButtonListener", Toast.LENGTH_SHORT).show();
-		RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-				R.layout.widget_layout);
+    private void updateWidgetPictureAndButtonListener(Context context) {
+        Toast.makeText(context, "begin updateWidgetPictureAndButtonListener", Toast.LENGTH_SHORT).show();
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                R.layout.widget_layout);
 
-		// updating view
-		remoteViews.setTextViewText(R.id.title, getTitle());
-		remoteViews.setTextViewText(R.id.desc, getDesc(context));
+        // updating view
+        remoteViews.setTextViewText(R.id.title, getTitle());
+        remoteViews.setTextViewText(R.id.desc, getDesc(context));
 
-		// re-registering for click listener
-		remoteViews.setOnClickPendingIntent(R.id.sync_button,
-				NewAppWidget.buildButtonPendingIntent(context));
+        // re-registering for click listener
+        remoteViews.setOnClickPendingIntent(R.id.sync_button,
+                NewAppWidget.buildButtonPendingIntent(context));
 
-		NewAppWidget.pushWidgetUpdate(context.getApplicationContext(),
-				remoteViews);
+        NewAppWidget.pushWidgetUpdate(context.getApplicationContext(),
+                remoteViews);
 //		Toast.makeText(context, "end updateWidgetPictureAndButtonListener", Toast.LENGTH_SHORT).show();
-	}
+    }
 
-	private String getDesc(Context context) {
-		// some static jokes from xml
-		msg = context.getResources().getStringArray(R.array.jokes);
-		if (clickCount >= msg.length) {
-			clickCount = 0;
-		}
-		return msg[clickCount];
-	}
+    private String getDesc(Context context) {
+        // some static jokes from xml
+        msg = context.getResources().getStringArray(R.array.jokes);
+        if (clickCount >= msg.length) {
+            clickCount = 0;
+        }
+        return msg[clickCount];
+    }
 
-	private String getTitle() {
-		return "Funny Jokes";
-	}
+    private String getTitle() {
+        return "Funny Jokes";
+    }
 }
