e3ff34db374731cb05127a66c81be512ecb691fa
David Strube <dstrube@gmail.com>
2018-11-20 11:57:14 -0500

    Dr Who via JFugue - initial commit, very much not done

---
+++ java/com/dstrube/jfugue/DrWho.java
@@ -0,0 +1,52 @@
+package com.dstrube.jfugue;
+
+/*
+commands to compile and run:
+
+from mac ~/java:
+compile:
+javac -cp bin:bin/jfugue-5.0.9.jar -d bin com/dstrube/jfugue/DrWho.java
+run:
+java -cp bin:bin/jfugue-5.0.9.jar com.dstrube.jfugue.DrWho
+
+*/
+
+import org.jfugue.player.Player;
+
+public class DrWho {
+
+/*
+Treble:
+FACE
+EGBDF - every good boy deserves fudge
+
+Bass:
+ACEG - all cows eat grass
+GBDFA - good boys do fine always
+
+whole w 
+half h 
+quarter q 
+eighth i 
+sixteenth s 
+thirty-second t 
+sixty-fourth x 
+one-twenty-eighth o
+*/
+	//Page 1, Measures 1-5
+	private static final String P1M1_5 = "C D E F G A B";//D3q E3i E3q D3i E3i E3i E3i D3i E3i E3i E3i";
+	public static void main(String[] args) {
+		Instruments();
+	}
+	
+	//1- Figure out what instruments to use
+	private static void Instruments(){
+		final int[] instruments = new int[128];
+		final Player player = new Player();
+		for (int i=0; i< instruments.length; i++){
+			instruments[i] = i;
+			System.out.println("i = "+i);
+			player.play("I"+instruments[i]+" " +P1M1_5);
+		}
+	}
+}
