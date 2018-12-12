975a8836927a31a01aad71abcf90140cac6351b4
David Strube <dstrube@gmail.com>
2018-12-12 00:52:42 -0500

    Renamed IntMax to Maths, to be more generic, so we can do more things that are interesting, and possibly more interesting things

--- java/com/dstrube/Maths.java
+++ java/com/dstrube/Maths.java
@@ -1,15 +1,18 @@
 /*
 From ~/java:
 
-javac -d bin com/dstrube/IntMax.java
-java -cp bin com.dstrube.IntMax
+javac -d bin com/dstrube/Maths.java
+java -cp bin com.dstrube.Maths
 
 */
 
 package com.dstrube;
 
-public class IntMax {
+public class Maths {
 	public static void main(String[] args) {
+		IntMax();
+	}
+	private static void IntMax(){
 		int i = 1;
 		int i_p = 0;
 		long count = 0;
@@ -17,11 +20,10 @@ public class IntMax {
 			i++;
 			i_p++;
 			count++;
-			if (count % 10000000 == 0)
+			if (count % 100000000 == 0)
 				System.out.print(".");
 		}
 		System.out.println();
 		System.out.println("Max of int found : " + count);
-	}
-	
+	}	
 }
