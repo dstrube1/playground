7510b7b9ac2f1eb2e41c2db744747b6df6554ca3
David Strube <dstrube@gmail.com>
2019-01-04 00:37:22 -0500

    Added multithreading for BigIntMax

--- java/com/dstrube/Maths.java
+++ java/com/dstrube/Maths.java
@@ -20,7 +20,7 @@ public class Maths {
 //		LongMax();	//9,223,372,036,854,775,807
 //		FloatMax(); //3.4028235E38
 //		DoubleMax(); //1.7976931348623157E308
-//		BigIntegerMax(); //complicated
+		BigIntegerMax(); //complicated
 	}
 	
     private static void ByteMax() {
@@ -80,8 +80,9 @@ public class Maths {
 			i++;
 			i_p++;
 			count++;
-			if (count % 100000000 == 0)
+			if (count % 100000000 == 0){
 				System.out.print(".");
+			}
 		}
 		System.out.println();
 		System.out.println("Max of int found : " + count);
@@ -283,10 +284,10 @@ public class Maths {
 
 	private static void BigIntegerMax(){
 		//https://docs.oracle.com/javase/7/docs/api/java/math/BigInteger.html
-		BigInteger bi = new BigInteger("1");
-		System.out.println("BigInteger 1 = " + bi.toString());	//1
-		bi = bi.multiply(BigInteger.TEN);
-		System.out.println("BigInteger 10 = " + bi.toString());	//10
+		//BigInteger bi = new BigInteger("1");
+		//System.out.println("BigInteger 1 = " + bi.toString());	//1
+		//bi = bi.multiply(BigInteger.TEN);
+		//System.out.println("BigInteger 10 = " + bi.toString());	//10
 		
 		//This seems to have no bottom, limited only by the amount of memory one has
 //		while(true){
@@ -294,8 +295,8 @@ public class Maths {
 //			System.out.println("BigInteger 10 = " + bi.toString());	
 //		}
 
-		bi = bi.pow(100);
-		System.out.println("BigInteger 10^100 = " + bi.toString());		//[10^100]
+		//bi = bi.pow(100);
+		//System.out.println("BigInteger 10^100 = " + bi.toString());		//[10^100]
 		
 		//This is weird:
 //		System.out.println("BigInteger intValue() = " + bi.intValue());	//0 ???
@@ -304,8 +305,8 @@ public class Maths {
 	
 		//Go big
 		//Reset to ten
-		bi = new BigInteger("1");
-		bi = bi.multiply(BigInteger.TEN);
+		//bi = new BigInteger("1");
+		//bi = bi.multiply(BigInteger.TEN);
 		//This doesn't break, but it also seems stuck; maybe try on a bigger machine
 		//System.out.println("BigInteger 10^IntMax... = ");		
 		//bi = bi.pow(Integer.MAX_VALUE);
@@ -325,10 +326,71 @@ public class Maths {
 //		System.out.println("BigInteger 10^1M = " + bi.toString());
 
 		//Takes a few seconds, but finishes
-		System.out.println("BigInteger 10^10M...(takes a few seconds) = ");		
-		bi = bi.pow(10000000); //10 ^ 10,000,000
-		System.out.println(bi.toString());
-		//TODO Is there a way to indicate its progress?
+		//System.out.println("BigInteger 10^10M...(takes a few seconds) = ");	
+		//bi = bi.pow(10000000); //10 ^ 10,000,000
+		//System.out.println(bi.toString());
+		//Is there a way to indicate its progress?
 		//probably not, but next best thing: timer with a separate thread printing out while counting to long max?
+		
+		final RunnableCounter runnableCounter = new RunnableCounter(); 
+        final Thread counterThread = new Thread(runnableCounter);//, "counterThread");
+        counterThread.start();
+        
+        final MultiThreadedBigIntegerTester biTester = new MultiThreadedBigIntegerTester();
+        biTester.start();
+        
+        while (biTester.isAlive()){}
+        
+        System.out.println("\n biTester is dead; interrupting counter...");
+        
+	    runnableCounter.stop();
 	}
 }
+
+class MultiThreadedBigIntegerTester extends Thread 
+{ 
+    public void run() 
+    { 
+        try
+        { 
+			BigInteger bi = new BigInteger("1");
+			bi = bi.multiply(BigInteger.TEN);
+			System.out.println("BigInteger 10^10M...(takes a few seconds) = ");
+			bi = bi.pow(10000000); //10 ^ 10,000,000
+			System.out.println(bi.toString());
+        } 
+        catch (Exception e) 
+        { 
+            // Throwing an exception 
+            System.out.println ("Exception is caught"); 
+        } 
+    } 
+} 
+
+class RunnableCounter implements Runnable 
+{ 
+	private volatile boolean exit = false;
+	
+    public void run() 
+    { 
+        try
+        { 
+			long count = 0;
+			while (!exit){
+				count++;
+				if (count % 100000000 == 0){
+					System.out.print(".");
+				}
+			}
+        } 
+        catch (Exception e) 
+        { 
+            // Throwing an exception 
+            System.out.println ("Exception is caught"); 
+        } 
+    } 
+    
+    public void stop(){
+    	exit = true;
+    }
+} 
