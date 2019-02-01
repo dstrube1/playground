a10e101e825d3bc1ee2c923edb0d48e48ee21f78
David Strube <dstrube@gmail.com>
2019-02-01 08:39:05 -0500

    Fixed and improved FolderComparer

--- java/com/dstrube/FolderComparer.java
+++ java/com/dstrube/FolderComparer.java
@@ -4,54 +4,82 @@ From ~/java:
 javac -d bin com/dstrube/FolderComparer.java
 java -cp bin com.dstrube.FolderComparer
 
-Phase 1 failed here:
-dir1List.length (11) != dir2List.length (14)
-dir1 = /Users/dstrube/Downloads/sort/AnimationsDemo/AnimationsDemo; 
-dir2 = /Users/dstrube/Downloads/sort/AnimationsDemo/AnimationsDemo2
-
-next, compare contents (phase 2) here:
-/Users/dstrube/Downloads/sort/AnimationsDemo/AnimationsDemo/src/com/example/android/animationsdemo
-/Users/dstrube/Downloads/sort/AnimationsDemo/AnimationsDemo2/app/src/main/java/com/dstrube/animationsdemo/app
+This compares two folders, first comparing the file counts, then the file contents.
 */
 
 package com.dstrube;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.FileInputStream;
 import java.util.Scanner;
 
 public class FolderComparer{
+
+	private static FilenameFilter fileNameFilter;
+
 	public static void main(String[] args){
+		if (args.length == 0){
+			System.out.println("Empty args");
+			return;
+		}
+		if (args.length == 1){
+			System.out.println("Not enough args");
+			return;
+		}
+		if (args.length > 2){
+			System.out.println("Too many args");
+			return;
+		}
 		try{
-			File file = new File("/Users/dstrube/Downloads/sort/AnimationsDemo");
-			//File dir1 = new File(file.getCanonicalPath()+"/AnimationsDemo");
-			//File dir2 = new File(file.getCanonicalPath()+"/AnimationsDemo2");
-			File dir1 = new File(file.getCanonicalPath()+"/AnimationsDemo/src/com/example/android/animationsdemo");
-			File dir2 = new File(file.getCanonicalPath()+"/AnimationsDemo2/app/src/main/java/com/dstrube/animationsdemo/app");
+			File dir1 = new File(args[0]);
+			File dir2 = new File(args[1]);
+			
+			//from https://www.tutorialspoint.com/java/io/file_listfiles_filename_filter.htm
+			fileNameFilter = new FilenameFilter() {
+   
+            	@Override
+	            public boolean accept(File dir, String name) {
+	            	if (dir.isDirectory()){
+	            		return true;
+	            	}
+    	           if(name.lastIndexOf('.') > 0) {
+               
+        	          // get last index for '.' char
+            	      int lastIndex = name.lastIndexOf('.');
+                  
+                	  // get extension
+	                  String str = name.substring(lastIndex);
+                  
+    	              // match path name extension
+        	          if(str.equals(".cs")) {
+            	         return true;
+	                  }
+    	           }
+               
+        	       return false;
+	            }
+    	     };
+			//end fileNameFilter
 			
 			if (phase1(dir1,dir2)){
 				System.out.println("Phase 1 passed, proceeding to phase 2...");
 			} else {
-				System.out.println("Phase 1 failed.");
+				//System.out.println("Phase 1 failed.");
 				return;
 			}
 	
 			if (phase2(dir1,dir2)){
 				System.out.println("Phase 2 passed.");
 			} else {
-				System.out.println("Phase 2 failed.");
+				//System.out.println("Phase 2 failed.");
 				return;
 			}
 			
-//        	System.out.println("file CanonicalPath: "+file.getCanonicalPath());
-//        	System.out.println("files listed: ");
-//    	    for (File f : file.listFiles()){
-//        	    System.out.println(f.getCanonicalPath());
-//	        }
-	    } catch (IOException ioe){
-	    	System.out.println("IOException");
+	    } catch (Exception exception){
+	    	System.out.println("Exception caught at " + methodName() + ": " + exception);
 	    }
 	}
 	
@@ -69,25 +97,38 @@ public class FolderComparer{
 		
 		try{
 			if (!dir1.exists()){
-				System.out.println("dir1 doesn't exist: "+dir1.getCanonicalPath());
+				System.out.println("dir1 doesn't exist: " + dir1);
 				return false;
 			}
 			if (!dir2.exists()){
-				System.out.println("dir2 doesn't exist: "+dir2.getCanonicalPath());
+				System.out.println("dir2 doesn't exist: " + dir2);
 				return false;
 			}
 			
 			//if dir1 and dir2 have different numbers of items, they're not equal
-			File[] dir1List = dir1.listFiles();
-			File[] dir2List = dir2.listFiles();
+			File[] dir1List = dir1.listFiles(fileNameFilter);
+			File[] dir2List = dir2.listFiles(fileNameFilter);
 			if (dir1List.length != dir2List.length)
 			{
-				System.out.println("dir1List.length ("+dir1List.length+") != dir2List.length ("+dir2List.length+")");
-				System.out.println("dir1 = "+dir1.getCanonicalPath()+"; dir2 = "+dir2.getCanonicalPath());
+				System.out.println("Failed base check");
+				System.out.println("dir1List.length (" + dir1List.length + ") != dir2List.length (" + dir2List.length + ")");
+				System.out.println("dir1 = " + dir1 
+					+ "\ndir2 = " + dir2);
+				System.out.println("dir1 contents:");
+				for (File file : dir1List)
+				{
+					System.out.println("-" + file);
+				}
+				System.out.println("dir2 contents:");
+				for (File file : dir2List)
+				{
+					System.out.println("-" + file);
+				}
+				
 				return false;
 			}
-	    } catch (IOException ioe){
-	    	System.out.println("IOException");
+	    } catch (Exception exception){
+	    	System.out.println("Exception caught at " + methodName() + ": " + exception);
 	    	return false;
 	    }
 		return true;
@@ -98,23 +139,27 @@ public class FolderComparer{
 		try{
 			//dir1 is a folder AND dir2 is NOT a folder
 			if (dir1.isDirectory() && !dir2.isDirectory()){
-				System.out.println("dir1.isDirectory("+dir1.getCanonicalPath()+") & NOT dir2.isDirectory("+dir2.getCanonicalPath()+")");
+				System.out.println("dir1.isDirectory(" + dir1 + ") & NOT dir2.isDirectory(" + dir2 + ")");
 				return false;
 			}
 
 			//dir1 is NOT a folder AND dir2 is a folder
-			if (! dir1.isDirectory() && dir2.isDirectory()){
-				System.out.println("NOT dir1.isDirectory("+dir1.getCanonicalPath()+") & dir2.isDirectory("+dir2.getCanonicalPath()+")");
+			if (!dir1.isDirectory() && dir2.isDirectory()){
+				System.out.println("NOT dir1.isDirectory(" + dir1 + ") & dir2.isDirectory(" + dir2 + ")");
 				return false;
 			}
-
+			
+			//both not folders - skip
+			if (! dir1.isDirectory() && !dir2.isDirectory()){
+				return true;
+			}
 			//dir1 name != dir2 name
 			if (!dir1.getName().equals(dir2.getName())){
-				System.out.println("dir1 name ("+dir1.getName()+") != dir2 name ("+dir2.getName()+")");
+				System.out.println("dir1 name (" + dir1.getName() + ") != dir2 name (" + dir2.getName() + ")");
 				return false;
 			}
-	    } catch (IOException ioe){
-	    	System.out.println("IOException");
+	    } catch (Exception exception){
+	    	System.out.println("Exception caught at " + methodName() + ": " + exception);
 	    	return false;
 	    }
 		return true;
@@ -122,32 +167,29 @@ public class FolderComparer{
 	
 	//Verify the folder structure is the same
 	private static boolean phase1(File dir1, File dir2){
-		//try{
+		if (!baseChecks(dir1, dir2)){
+	    	return false;
+		}
 			
-			if (!baseChecks(dir1, dir2)){
-				System.out.println("Failed phase 1 base check.");
+		File[] dir1List = dir1.listFiles(fileNameFilter);
+		File[] dir2List = dir2.listFiles(fileNameFilter);
+
+		for (int i = 0; i < dir1List.length; i++){
+			if(!baseForChecks(dir1List[i], dir2List[i])){
+				System.out.println("Failed phase 1 base For check.");
 		    	return false;
 			}
-			
-			File[] dir1List = dir1.listFiles();
-			File[] dir2List = dir2.listFiles();
-
-			for (int i=0; i<dir1List.length; i++){
-				if(!baseForChecks(dir1List[i], dir2List[i])){
-					System.out.println("Failed phase 1 base For check.");
-			    	return false;
-				}
 				
-				//recurse
-				if (dir1List[i].isDirectory() && dir2List[i].isDirectory()){
-					return phase1(dir1List[i], dir2List[i]);
+			//recurse
+			if (dir1List[i].isDirectory() && dir2List[i].isDirectory()){
+				if (phase1(dir1List[i], dir2List[i])){
+					continue;
+				} else{
+					return false;
 				}
-			}//end for
+			}
+		}//end for
 
-	    /*} catch (IOException ioe){
-	    	System.out.println("IOException");
-	    	return false;
-	    }*/
 		return true;
 	}
 
@@ -160,10 +202,10 @@ public class FolderComparer{
 		    	return false;
 			}
 			
-			File[] dir1List = dir1.listFiles();
-			File[] dir2List = dir2.listFiles();
+			File[] dir1List = dir1.listFiles(fileNameFilter);
+			File[] dir2List = dir2.listFiles(fileNameFilter);
 			
-			for (int i=0; i<dir1List.length; i++){
+			for (int i = 0; i < dir1List.length; i++){
 				if(!baseForChecks(dir1List[i], dir2List[i])){
 					System.out.println("Failed phase 2 base For check.");
 			    	return false;
@@ -171,53 +213,72 @@ public class FolderComparer{
 				
 				//recurse
 				if (dir1List[i].isDirectory() && dir2List[i].isDirectory()){
-					return phase2(dir1List[i], dir2List[i]);
+					if (!phase2(dir1List[i], dir2List[i])){
+						return false;
+					}
+				}
+				
+				//One of these is a dir; skip
+				if (dir1List[i].isDirectory() || dir2List[i].isDirectory()){
+					continue;
 				}
 				
 				//Now we're dealing with 2 files
 				FileInputStream fileInputStream1 = new FileInputStream(dir1List[i]);
 				FileInputStream fileInputStream2 = new FileInputStream(dir2List[i]);
-				
+
 				Scanner scanner1 = new Scanner(fileInputStream1);
 				Scanner scanner2 = new Scanner(fileInputStream2);
+				long lineNumber = 1;
 				while(areScannersGood(scanner1, scanner2)){
-					String line1 = scanner1.next();
-					String line2 = scanner2.next();
+					String line1 = scanner1.nextLine();
+					String line2 = scanner2.nextLine();
+
 					if (! line1.equals(line2)){
-						System.out.println("Failed phase 2: at files \n"+
-						dir1List[i].getCanonicalPath() + " \nand \n" + dir2List[i].getCanonicalPath()+
-						"\nthese lines don't match: file1: \n"+
-						line1 +" \nfile2: \n"+ line2);
+						System.out.println("Failed phase 2: at these files \n" 
+							+ "1: " + dir1List[i] + "\n"
+							+ "2: " + dir2List[i] 
+							+ "\nLines (#" + lineNumber + ") don't match: \n" 
+							+ "1: " + line1 + "\n"
+							+ "2: " + line2);
 						return false;
 					}
-				}
-				
-			}
+					lineNumber++;
+				}//end while
+				scanner1.close();
+				scanner2.close();
+			}//end for
 
 	    } catch (FileNotFoundException fnfe){
 	    	System.out.println("FileNotFoundException");
 	    	return false;
-	    } catch (IOException ioe){
-	    	System.out.println("IOException");
-	    	return false;
-	    }
+	    } 
 		return true;
 	}
 	
 	private static boolean areScannersGood(Scanner scanner1, Scanner scanner2){
-		if (!scanner1.hasNext() && scanner2.hasNext()){
+		if (!scanner1.hasNextLine() && scanner2.hasNextLine()){
 			System.out.println("Scanner 1 NOT has next AND scanner 2 has next.");
 	    	return false;
 		}
-		if (scanner1.hasNext() && !scanner2.hasNext()){
-			System.out.println("Scanner 1 NOT has next AND scanner 2 has next.");
+		if (scanner1.hasNextLine() && !scanner2.hasNextLine()){
+			System.out.println("Scanner 1 has next AND scanner 2 NOT has next.");
 	    	return false;
 		}
-		if (!scanner1.hasNext() && !scanner2.hasNext()){
-	    	return true;
+		if (!scanner1.hasNextLine() && !scanner2.hasNextLine()){
+	    	return false;
 		}
 
 		return true;
 
+	}
+	
+	private static String methodName() {
+		//https://www.baeldung.com/java-name-of-executing-method
+	    String methodName = 
+    	  Thread.currentThread()
+    	  .getStackTrace()[2]//0 = "getStackTrace"; 1 = "methodName"; 2 = calling method
+    	  .getMethodName();
+        return methodName;
 	}
 }
