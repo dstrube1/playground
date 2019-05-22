a95e7eb32f5bc164dd82e471da3819db597612ef
David Strube <dstrube@gmail.com>
2019-05-22 08:18:01 -0400

    Moved java code from playground to playground_java

--- java/com/dstrube/misc/GenericDataRow.java
+++
@@ -1,9 +0,0 @@
-package com.dstrube;
-
-public class GenericDataRow {
-	public String identifier = "";
-	public int[] columns = new int[9];
-	public int dataDiff = 0;
-	
-	public GenericDataRow(){}
-}
