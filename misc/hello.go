4a5f1dd1338a412c68c60c36848cc972533e2967
David Strube <dstrube3@gatech.edu>
2021-11-01 08:40:44 -0400

    Create hello.go

---
+++ misc/hello.go
@@ -0,0 +1,41 @@
+package main
+
+//1 import:
+//import "fmt"
+
+//multiple imports:
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello, 世界")
+	fmt.Println("The time is", time.Now())
+}
+/*
+Filled out info honestly
+gave phone number
+gave details (state should be drop down)
+Then:
+Based on your responses, we recommend you to visit a local clinic for a thorough evaluation and in person support.
+(add google maps link with search for local clinics)
+Why not give this result before more details?
+
+Filled out dummy info
+gave phone number
+then got appt info
+
+Next past Dec 28= blank screen
+
+found in 2019
+18 employees
+based in SF
+33 states
+20k patients -> 30k over the past few weeks
+
+Hiring manager:
+Michael Poon
+https://www.linkedin.com/in/mpoon1/
+
+*/
