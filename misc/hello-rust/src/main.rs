62b42eff564214729cfacd14c79916766e58237a
David Strube <dstrube3@gatech.edu>
2023-03-03 19:50:31 -0500

    Fixed hello-rust (thanks to ChatGPT)

--- misc/hello-rust/src/main.rs
+++ misc/hello-rust/src/main.rs
@@ -1,13 +1,3 @@
-//https://www.rust-lang.org/learn/get-started
-
-fn main() {
-    println!("Hello, world!");
-//	func_1();
-}
-
-/*
-//https://www.youtube.com/watch?v=pTMvh6VzDls
-//17:21
 struct Bob{
 	n: Vec<i32>
 }
@@ -18,14 +8,47 @@ impl Bob{
 }
 
 fn func_1(){
-	let n = Bob::new();
-	func_2(&n);
+	let bob = Bob::new();
+	let bob1 = &bob;
+	let bob2 = &bob;
+	//Can't* do both mutable and immutable, 
+	//	(*unless put into a special block that's marked "unsafe"? 22:27)
+	//let bob3 = &mut bob;
+	func_2(bob1);
+	func_3(bob2);
+	//func_4(bob3);
 	println!("{}", bob.n[0]);
 }
 
-fn func_2(b: &Bob){
+fn func_2(bob: &Bob){
 	println!("{}", bob.n[0]);
 }
-*/
+
+fn func_3(bob: &Bob){
+	println!("{}", bob.n[1]);
+}
+
+/*fn func_4(bob: &mut Bob){
+	bob.n = Vec::new();
+}*/
+
+fn main() {
+	func_1();
+}
+
+
+//https://www.rust-lang.org/learn/get-started
+
+//to build, from hello-rust dir:
+//cargo build
+
+//to run:
+//cargo run
+
+//https://www.youtube.com/watch?v=pTMvh6VzDls
+//17:21
+/**/
 // 17:54 "And then if I was to compile this func_1 now, it would compile absolutely fine. No issues." 
 //False
+
+//Solution: ask ChatGPT to debug, and it gave the answer!
