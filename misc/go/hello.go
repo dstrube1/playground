05830477ef791bc4de283e0a4fc3adb9f0fa64d8
David Strube <dstrube3@gatech.edu>
2025-06-04 08:25:18 -0400

    Revised go code structure

---
+++ misc/go/hello.go
@@ -0,0 +1,22 @@
+package main
+
+//to run:
+//go run hello.go
+
+//1 import:
+//import "fmt"
+
+//multiple imports:
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello, 世界")
+	fmt.Println("The time is", time.Now())
+}
+/*
+
+
+*/
