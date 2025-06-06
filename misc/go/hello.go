903e9339c5bd6618c5a4a993674a6b71a9e2829c
David Strube <dstrube3@gatech.edu>
2025-06-06 13:24:51 -0400

    Progress

--- misc/go/hello.go
+++ misc/go/hello.go
@@ -2,6 +2,8 @@ package main
 
 //to run:
 //go run hello.go
+//to save as executable:
+//go build hello.go
 
 //1 import:
 //import "fmt"
@@ -13,8 +15,69 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, 世界")
+	//statements are separaated by newlines or ;
+	//line cannot begin with {; eg, try commenting out the one above and uncommenting this:
+	//{
+	fmt.Println("Hello, 世界") //Chinese for "world"
 	fmt.Println("The time is", time.Now())
+	
+	//variables:
+	var i int //default: 0
+	//Go really doesn't like variables that are declared and not used
+	var b bool //default: false
+	var f float32 //default: 0
+	var s string = "s" //must use ", not '; default: ""
+	
+	//can declare like one of these:
+	//a- var varname type = value
+	//b- var varname type
+	//c- var varname = value
+	//d- varname := value 
+	//c & d: go figures out the type
+	
+	//If you use the type keyword, it is only possible to declare one type of variable per line.
+	//If the type keyword is not specified, you can declare different types of variables on the same line:
+	//var w, x = 6, "Hello"
+	//y, z := 7, "World!"
+	
+	/*i0 := 1
+	b0 := true
+	f0 := 2.0
+	s0 := "s0"*/
+	
+	
+	fmt.Print("i: ")
+	fmt.Println(i)/*
+	fmt.Print("b: ")
+	fmt.Print("f: ")
+	*/
+	//arguments to Print separated by ,
+	fmt.Print("s: " + s, "\n")
+	//if neither argument is string, then Print will insert a space between each
+	fmt.Print("Default values of bool and float32: ")
+	fmt.Print(b,f)
+	//neither Print nor Println require an argument:
+	fmt.Print()
+	fmt.Println()
+	
+	//constant:
+	//const PI = 3.14
+	//is it possible to declare a constant with a type and without a value, as long as it's used?	
+	/*const PI float32
+	fmt.Print("PI: ")		
+	fmt.Println(PI)*/
+	//no
+
+	//using Printf:
+	fmt.Printf("i has value: %v and type: %T\n", i, i)
+	fmt.Printf("s has value: %v and type: %T\n", s, s)
+	//more: https://www.w3schools.com/go/go_formatting_verbs.php
+	
+	//more Numerics:
+	var u uint64 = 18446744073709551615 //18,446,744,073,709,551,615 =~ 18 quintillion
+	fmt.Println("u: ", u)
+	
+	//Next, arrays...
 }
 /*
 
