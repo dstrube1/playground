1050b0d0737b20f635fe2bf0020cf1226f599379
David Strube <dstrube3@gatech.edu>
2025-08-25 09:34:39 -0400

    Update pi.py

--- rust/PyO3/py03-example/pi.py
+++ rust/PyO3/py03-example/pi.py
@@ -3,9 +3,10 @@
 import py03_example
 
 sum_string = py03_example.sum_as_string(5,20)
-pi_approximation = py03_example.calculate_pi(10000)
-print("sum as string: " + sum_string)
-print("pi approximation: " + str(pi_approximation))
+iterations = 100_000
+pi_approximation = py03_example.calculate_pi(iterations)
+print("sum as string (5,20): " + sum_string)
+print("pi approximation (with " + str(iterations) + " iterations): " + str(pi_approximation))
 
 # Hmm, ./pi.py doesn't work as well IRL as it does in the tutorial video :/
 
