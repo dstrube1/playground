ec8fee918d030d268c817c72f442da07e3820a6c
David Strube <dstrube3@gatech.edu>
2025-09-08 22:22:55 -0400

    Update lib.rs

--- rust/PyO3/pycalc/src/lib.rs
+++ rust/PyO3/pycalc/src/lib.rs
@@ -1,5 +1,5 @@
 use pyo3::prelude::*;
-use pyo3::
+use pyo3::exceptions::PyZeroDivisionError;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -18,15 +18,27 @@ fn product_as_string(a: usize, b: usize) -> PyResult<String> {
 }
 
 #[pyfunction]
-fn quotient_as_string(a: f64, b: f64) -> PyResult<String> {
+fn quotient_as_string(a: f64, b: f64) -> PyResult<f64> {
 	// What if b == 0? Raise an error / throw an exception:
 	if b == 0.0 {
 		//https://www.linkedin.com/learning/using-rust-with-python/pyo3-exceptions
 		//Example in the video requires a working pycalc project -_-
-		return Err(PyValueError::new_err("Can't divide by zero'"))
+		//return Err(PyValueError::new_err("Can't divide by zero'"))
 		// PyErr::new_instance(PyExc_ValueError,("Invalid value",)).unwrap().raise(py)
+		return Err(PyZeroDivisionError::new_err("Exception: !Division by zero"));
+		/*
+		USAGE:
+		from libpycalc_cli import...
+		
+		print("divide 10 / 2: " + str(divide(10,2)))
+		print("divide 10 / 0: ...")
+		try:
+			print(divide(10,0))
+		except ZeroDivisionError as zde: //ZeroDivisionError included in libpycalc_cli?
+			print("Exception from Rust: {zde}")
+		*/
 	}
-    Ok((a / b).to_string())
+    Ok((a / b))
 }
 
 /// A Python module implemented in Rust.
