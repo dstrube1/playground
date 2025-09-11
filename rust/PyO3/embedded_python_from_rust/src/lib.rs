db0f21e1a695f2a4357fda818851af71ee915457
David Strube <dstrube3@gatech.edu>
2025-09-11 09:34:29 -0400

    embedded python from rust

---
+++ rust/PyO3/embedded_python_from_rust/src/lib.rs
@@ -0,0 +1,53 @@
+use pyo3::types::PyString;
+use pyo3::{prelude::*, types::PyModeule};
+
+/*
+from https://www.linkedin.com/learning/using-rust-with-python/run-python-with-embedded-rust
+
+Must use the {maturin init} command from the embedded_python_from_rust directory, already existing and empty.
+
+*/
+
+fn marco_python(input: &str) -> PyResult<String>{
+	pyo3::prepare_freethreaded_python();
+	Python::with_gil(|py| {
+		let marco = PyModule::from_code(
+			py,
+			r#"
+def marco(input):
+	if input == "marco":
+		return "python"
+	else:
+		return "no python"
+"#,
+			"marco.py",
+			"marco",
+		)?;
+		let marco_func = marco.getattr("marco")?;
+		let marco_result = marco_func.call1((input,))?;
+		let marco_result: &PyString = marco_result.extract()?;
+		Ok(marco_result.to_string())
+	})
+}
+
+fn main(){
+	println!("From embedded Python: {}", marco_python("marco").unwrap());
+	println!("From embedded Python: {}", marco_python("polo").unwrap());
+}
+
+/*
+TODO: above doesn't build, much less run
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn embedded_python_from_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
+*/
